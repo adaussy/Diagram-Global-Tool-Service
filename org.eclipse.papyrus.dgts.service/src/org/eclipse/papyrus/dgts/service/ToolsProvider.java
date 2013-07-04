1765f414d16a10d3ac4f81a89aeaa6efe913607b
guilhem.desq <guilhem.desq@gmail.com>
2013-07-04 15:32:46 +0200

    tout les services implémentés !

--- org.eclipse.papyrus.dgts.service/src/org/eclipse/papyrus/dgts/service/ToolsProvider.java
+++ org.eclipse.papyrus.dgts.service/src/org/eclipse/papyrus/dgts/service/ToolsProvider.java
@@ -1,27 +1,54 @@
 package org.eclipse.papyrus.dgts.service;
 
+import java.util.ArrayList;
 import java.util.List;
-
-
 import org.eclipse.papyrus.dgts.service.interfaces.IToolsProvider;
-
 import DiagramGlobalToolService.DiagramDefinition;
+import DiagramGlobalToolService.DiagramGlobalToolDefinition;
 import DiagramGlobalToolService.DrawerDefinition;
 import DiagramGlobalToolService.ToolElement;
+
 public class ToolsProvider implements IToolsProvider {
 
-	//get all tools from a diagram
-	public List<ToolElement> getTools(DiagramDefinition diagram){
+	// get Diagram from diagram string and resource
+	public DiagramDefinition getDiagram(String diagramType,
+			DiagramGlobalToolDefinition global) {
+		for (DiagramDefinition diagram : global.getDiagramDefinitionRef()) {
+			if (diagram.getDiagramType() == diagramType) {
+				return diagram;
+			}
+		}
 		return null;
 	}
-	
-	//get all tools from a given diagram and a given drawer
-	public List<ToolElement> getTools(DiagramDefinition diagram, DrawerDefinition drawer){
-		return null;
+
+	// get all tools from a diagram
+	public List<ToolElement> getTools(DiagramDefinition diagram) {
+
+		List<ToolElement> toolList = new ArrayList<ToolElement>();
+		for (DrawerDefinition drawer : diagram.getDrawerDefinitionRef()) {
+			for (ToolElement tool : drawer.getToolElementRef()) {
+				toolList.add(tool);
+			}
+		}
+		return toolList;
 	}
-	
-	//get all drawers from a diagram
-	public List<DrawerDefinition> getDrawers(DiagramDefinition diagram){
-		return null;
+
+	// get all tools from a given drawer
+	public List<ToolElement> getTools(DrawerDefinition drawer) {
+
+		List<ToolElement> toolList = new ArrayList<ToolElement>();
+		for (ToolElement tool : drawer.getToolElementRef()) {
+			toolList.add(tool);
+		}
+		return toolList;
+	}
+
+	// get all drawers from a diagram
+	public List<DrawerDefinition> getDrawers(DiagramDefinition diagram) {
+		List<DrawerDefinition> drawerList = new ArrayList<DrawerDefinition>();
+		for (DrawerDefinition drawer : diagram.getDrawerDefinitionRef()) {
+			drawerList.add(drawer);
+		}
+		return drawerList;
 	}
 }
--- org.eclipse.papyrus.dgts.service/src/org/eclipse/papyrus/dgts/service/interfaces/IToolsProvider.java
+++ org.eclipse.papyrus.dgts.service/src/org/eclipse/papyrus/dgts/service/interfaces/IToolsProvider.java
@@ -1,20 +1,23 @@
 package org.eclipse.papyrus.dgts.service.interfaces;
 
 import java.util.List;
-
 import DiagramGlobalToolService.DiagramDefinition;
+import DiagramGlobalToolService.DiagramGlobalToolDefinition;
 import DiagramGlobalToolService.DrawerDefinition;
 import DiagramGlobalToolService.ToolElement;
 
 public interface IToolsProvider {
+	// get Diagram from diagram string and resource
+	public DiagramDefinition getDiagram(String diagramType,
+			DiagramGlobalToolDefinition global);
 
-	//get all tools from a diagram
+	// get all tools from a diagram
 	public abstract List<ToolElement> getTools(DiagramDefinition diagram);
 
-	//get all tools from a given diagram and a given drawer
-	public abstract List<ToolElement> getTools(DiagramDefinition diagram,DrawerDefinition drawer);
+	// get all tools from a given drawer
+	public abstract List<ToolElement> getTools(DrawerDefinition drawer);
 
-	//get all drawers from a diagram
+	// get all drawers from a diagram
 	public abstract List<DrawerDefinition> getDrawers(DiagramDefinition diagram);
 
 }
