a80474312e1d7e577fce3ba397cdf41393266828
guilhem.desq <guilhem.desq@gmail.com>
2013-07-04 14:13:08 +0200

    load resource
    Signed-off-by: Vincent LARTIGAUT <vlartiga@AOFR37777.TOULOUSE>

--- org.eclipse.papyrus.dgts.service/src/org/papyrus/dgts/service/DgtsResourceLoader.java
+++ org.eclipse.papyrus.dgts.service/src/org/papyrus/dgts/service/DgtsResourceLoader.java
@@ -2,12 +2,25 @@ package org.papyrus.dgts.service;
 
 
 
-import org.osgi.resource.Resource;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.*;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 public class DgtsResourceLoader implements IDgtsResourceLoader {
 
-	public void LoadResource(Resource resource){
-	
+	public void LoadResource(IFile file) throws IOException{
+		  URI uri = URI.createFileURI(file.getFullPath().toString());
+		  ResourceSet resourceSet = new ResourceSetImpl();
+		  org.eclipse.emf.ecore.resource.Resource resource = resourceSet.getResource(uri, true);
+		  Map<Object,Object> options = new HashMap<Object,Object>();
+		  resource.load(options);
+
 	}
+	
+	
 
 }
--- org.eclipse.papyrus.dgts.service/src/org/papyrus/dgts/service/IDgtsResourceLoader.java
+++ org.eclipse.papyrus.dgts.service/src/org/papyrus/dgts/service/IDgtsResourceLoader.java
@@ -1,9 +1,13 @@
 package org.papyrus.dgts.service;
 
-import org.osgi.resource.Resource;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IFile;
+
+
 
 public interface IDgtsResourceLoader {
 	
-	public void LoadResource(Resource resource);
+	public void LoadResource(IFile file) throws IOException;
 		
 }
