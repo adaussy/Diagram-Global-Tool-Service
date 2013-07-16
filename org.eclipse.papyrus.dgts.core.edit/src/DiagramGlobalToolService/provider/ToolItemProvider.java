144cba003c77a7e3e21ab5728042f38d7c9468f2
guilhem.desq <guilhem.desq@gmail.com>
2013-07-16 14:43:41 +0200

    model redef FInal FINAL

--- org.eclipse.papyrus.dgts.core.edit/src/DiagramGlobalToolService/provider/ToolItemProvider.java
+++ org.eclipse.papyrus.dgts.core.edit/src/DiagramGlobalToolService/provider/ToolItemProvider.java
@@ -3,22 +3,21 @@
 package DiagramGlobalToolService.provider;
 
 
+import DiagramGlobalToolService.DiagramGlobalToolServiceFactory;
 import DiagramGlobalToolService.DiagramGlobalToolServicePackage;
 import DiagramGlobalToolService.Tool;
-
 import java.util.Collection;
 import java.util.List;
-
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
-import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ViewerNotification;
 
 /**
  * This is the item provider adapter for a {@link DiagramGlobalToolService.Tool} object.
@@ -55,31 +54,38 @@ public class ToolItemProvider
                 if (itemPropertyDescriptors == null) {
                         super.getPropertyDescriptors(object);
 
-                        addElementTypesPropertyDescriptor(object);
                 }
                 return itemPropertyDescriptors;
         }
 
         /**
-         * This adds a property descriptor for the Element Types feature.
+         * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+         * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+         * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
          * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
          * @generated
          */
-        protected void addElementTypesPropertyDescriptor(Object object) {
-                itemPropertyDescriptors.add
-                        (createItemPropertyDescriptor
-                                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                                 getResourceLocator(),
-                                 getString("_UI_Tool_elementTypes_feature"),
-                                 getString("_UI_PropertyDescriptor_description", "_UI_Tool_elementTypes_feature", "_UI_Tool_type"),
-                                 DiagramGlobalToolServicePackage.Literals.TOOL__ELEMENT_TYPES,
-                                 true,
-                                 false,
-                                 true,
-                                 null,
-                                 null,
-                                 null));
+        @Override
+        public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+                if (childrenFeatures == null) {
+                        super.getChildrenFeatures(object);
+                        childrenFeatures.add(DiagramGlobalToolServicePackage.Literals.TOOL__ELEMENT_TYPES);
+                }
+                return childrenFeatures;
+        }
+
+        /**
+         * <!-- begin-user-doc -->
+         * <!-- end-user-doc -->
+         * @generated
+         */
+        @Override
+        protected EStructuralFeature getChildFeature(Object object, Object child) {
+                // Check the type of the specified child object and return the proper feature to use for
+                // adding (see {@link AddCommand}) it as a child.
+
+                return super.getChildFeature(object, child);
         }
 
         /**
@@ -117,6 +123,12 @@ public class ToolItemProvider
         @Override
         public void notifyChanged(Notification notification) {
                 updateChildren(notification);
+
+                switch (notification.getFeatureID(Tool.class)) {
+                        case DiagramGlobalToolServicePackage.TOOL__ELEMENT_TYPES:
+                                fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+                                return;
+                }
                 super.notifyChanged(notification);
         }
 
@@ -130,6 +142,11 @@ public class ToolItemProvider
         @Override
         protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
                 super.collectNewChildDescriptors(newChildDescriptors, object);
+
+                newChildDescriptors.add
+                        (createChildParameter
+                                (DiagramGlobalToolServicePackage.Literals.TOOL__ELEMENT_TYPES,
+                                 DiagramGlobalToolServiceFactory.eINSTANCE.createElementType()));
         }
 
 }
