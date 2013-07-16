d4aa81c6281b0c92cf36b01eaecabbf6bae901cd
Vincent LARTIGAUT <vlartiga@AOFR37777.TOULOUSE>
2013-07-16 15:50:21 +0200

    Commit handler

--- org.eclipse.papyrus.dgts.commands/src/org/eclipse/papyrus/dgts/commands/LoadModelCommandHandler.java
+++ org.eclipse.papyrus.dgts.commands/src/org/eclipse/papyrus/dgts/commands/LoadModelCommandHandler.java
@@ -1,30 +1,17 @@
 package org.eclipse.papyrus.dgts.commands;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.gef.palette.PaletteRoot;
-import org.eclipse.gmf.runtime.diagram.ui.DiagramUtil;
-import org.eclipse.gmf.runtime.diagram.ui.parts.IDiagramWorkbenchPart;
-import org.eclipse.gmf.runtime.diagram.ui.services.palette.PaletteService;
-import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.jface.viewers.IStructuredSelection;
-
 import org.eclipse.papyrus.dgts.service.DgtsResourceLoader;
 import org.eclipse.papyrus.dgts.service.ToolDefinitionResourceProvider;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.commands.ICommandService;
 
 public class LoadModelCommandHandler extends AbstractHandler {
 	protected static final String EXTENSION = "diagramglobaltoolservice";
@@ -46,7 +33,7 @@ public class LoadModelCommandHandler extends AbstractHandler {
 				Object firstElement = structuredSelection.getFirstElement();
 				if (firstElement instanceof IFile) {
 					IFile file = (IFile) firstElement;
-					if (IExtenstionPath.EXTENSION.equals(file
+					if (EXTENSION.equals(file
 							.getFileExtension())) {
 						try {
 							Resource resource = DgtsResourceLoader
