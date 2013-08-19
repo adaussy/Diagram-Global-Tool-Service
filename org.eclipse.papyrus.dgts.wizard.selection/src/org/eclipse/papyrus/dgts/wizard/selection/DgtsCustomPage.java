6740c1ae60e373b1b3c656d9660b743c2d4cc869
vlartiga <vincent.lartigaut@atos.net>
2013-08-19 09:29:31 +0200

    Last commit with DGTS model configuration selection wizard modification

--- org.eclipse.papyrus.dgts.wizard.selection/src/org/eclipse/papyrus/dgts/wizard/selection/DgtsCustomPage.java
+++ org.eclipse.papyrus.dgts.wizard.selection/src/org/eclipse/papyrus/dgts/wizard/selection/DgtsCustomPage.java
@@ -18,25 +18,22 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.papyrus.dgts.service.IDGTSExtensionDefinition;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -48,27 +45,38 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.PlatformUI;
 import org.osgi.framework.Bundle;
 
-/** Page present in the DGTS Selection wizard which allow the user to select a custom DGTS model configuration
+/**
+ * Page present in the DGTS Selection wizard which allow the user to select a custom DGTS model configuration
+ * 
  * @author vlartiga
- *
+ * 
  */
-public class DgtsCustomPage extends WizardPage{
+public class DgtsCustomPage extends WizardPage {
 
 	private Composite container;
+
 	private TableViewer toolSelectionTableViewer;
+
 	private Table selectionTable;
+
 	private Combo combo;
+
 	protected List<String> listPath = new ArrayList<String>();
+
 	private DiagramEditPart editPart;
 
+	private IStructuredSelection currentSelection = null;
+
+	private Button deletModelConfiguration;
+
 	protected DgtsCustomPage(DiagramEditPart editPart) {
-		
+
 		super("Select DGTS Model Configuration");
 
 		setTitle("Select DGTS Model Configuration");
 
 		setDescription("Select DGTS Model Configuration");
-		this.editPart= editPart ;
+		this.editPart = editPart;
 	}
 
 	@Override
@@ -78,174 +86,196 @@ public class DgtsCustomPage extends WizardPage{
 		container.setLayout(layout);
 		layout.numColumns = 1;
 		setControl(container);
-		try {
-			createModelConfigurationList(container) ;
-		} catch (CoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
+		GridLayout gridLayout1 = new GridLayout(2, false);
+		Composite addanddeleteComposite = new Composite(container, SWT.NONE);
+		addanddeleteComposite.setLayout(gridLayout1);
+		createLoadModelButton(addanddeleteComposite);
+		createDeleteButton(addanddeleteComposite);
+
+
+		GridLayout gridLayout2 = new GridLayout(2, false);
+		Composite addpredefinedContainer = new Composite(container, SWT.NONE);
+		addpredefinedContainer.setLayout(gridLayout2);
+
+		combo = new Combo(addpredefinedContainer, Combo.LIMIT);
+		combo.setText("select predefined model configuration");
+
+		addPredefiniedModelConfigurationFromPreferences(combo);
+		createAddPreferencesButton(addpredefinedContainer);
+		createModelConfigurationList(container);
 		setPageComplete(false);
-		
+
 	}
 
-	protected void createModelConfigurationList(Composite container) throws CoreException {
+	protected void createModelConfigurationList(Composite container) {
 		GridData selectionData = new GridData(SWT.FILL, SWT.FILL, true, true);
 		selectionData.heightHint = 300;
-		Group group = createGroup(container, "Model configuration list") ;
-		 selectionTable = new Table(group , SWT.NO_BACKGROUND);
+		Group group = createGroup(container, "Model configuration list");
+		selectionTable = new Table(group, SWT.NO_BACKGROUND);
 		selectionTable.setFont(group.getFont());
 		selectionTable.setBackground(group.getBackground());
 		selectionTable.setLayoutData(selectionData);
+		selectionTable.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				currentSelection = (StructuredSelection)toolSelectionTableViewer.getSelection();
+				deletModelConfiguration.setEnabled(true);
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 		toolSelectionTableViewer = new TableViewer(selectionTable);
 		initToolSelectionTableViewer(toolSelectionTableViewer);
-		createDeleteButton(container);
-		createLoadModelButton(container);
-		 combo = new Combo(container, Combo.LIMIT );
-		combo.setText("select predefined model configuration");
-		addPredefiniedModelConfigurationFromPreferences(combo) ;
-		createAddPreferencesButton(container);
+
 
 	}
 
 	protected void createLoadModelButton(Composite container) {
 		Button loadPersonnalModelConfiguration = new Button(container, SWT.NONE);
-		loadPersonnalModelConfiguration.setText("Load Personnal Model Configuration");
-			loadPersonnalModelConfiguration.addSelectionListener(new SelectionListener() {
-			
+		loadPersonnalModelConfiguration.setToolTipText("Add Dgts configuration from file");
+		Image image = new Image(null, getClass().getResourceAsStream("icons/editconfiguration.png"));
+		loadPersonnalModelConfiguration.setImage(image);
+		loadPersonnalModelConfiguration.addSelectionListener(new SelectionListener() {
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				FileDialog dialog = new FileDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), SWT.NULL);
 				String path = dialog.open();
-				if (path != null) {
+				if(path != null) {
 					toolSelectionTableViewer.add(path);
 					listPath.add(path);
 				}
-				
+
 			}
-			
+
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
 	}
 
 	protected void createAddPreferencesButton(Composite container) {
 		Button addPredefined = new Button(container, SWT.NONE);
-		addPredefined.setText("Add Predefined Element");
+		addPredefined.setText("Add a registred Dgts configuration");
 		addPredefined.addMouseListener(new MouseListener() {
-			
+
 
 			@Override
 			public void mouseUp(MouseEvent e) {
-				if(!"".equals(combo.getText())&& ! listPath.contains(combo.getText())){
-					listPath.add(combo.getText()) ;
+				if(!"".equals(combo.getText()) && !listPath.contains(combo.getText())) {
+					listPath.add(combo.getText());
 					toolSelectionTableViewer.add(combo.getText());
 				}
-				
-				
-				
+
+
+
 			}
-			
+
 			@Override
 			public void mouseDown(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseDoubleClick(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
 	}
 
 	protected void createDeleteButton(Composite container) {
-		Button deletModelConfiguration = new Button(container, SWT.NONE);
-		deletModelConfiguration.setText("Delete model Configuration");
+		deletModelConfiguration = new Button(container, SWT.NONE);
+		deletModelConfiguration.setEnabled(false);
+		deletModelConfiguration.setToolTipText("Delete model Configuration");
+		Image image = new Image(null, getClass().getResourceAsStream("icons/delete.png"));
+		deletModelConfiguration.setImage(image);
 		deletModelConfiguration.addMouseListener(new MouseListener() {
-			
+
 			@Override
 			public void mouseUp(MouseEvent e) {
-				StructuredSelection selection =  (StructuredSelection) toolSelectionTableViewer.getSelection();
-				listPath.remove(selection.getFirstElement());
-				toolSelectionTableViewer.remove(selection.getFirstElement());
-				
+				if(currentSelection != null) {
+					listPath.remove(currentSelection.getFirstElement());
+					toolSelectionTableViewer.remove(currentSelection.getFirstElement());
+					deletModelConfiguration.setEnabled(false);
+				}
 			}
-			
+
 			@Override
 			public void mouseDown(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseDoubleClick(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
 	}
 
-		private void initToolSelectionTableViewer(
-			TableViewer toolSelectionTableViewer2) {
-		for(EAnnotation annotation : this.editPart.getDiagramView().getEAnnotations()){
+	private void initToolSelectionTableViewer(TableViewer toolSelectionTableViewer2) {
+		for(EAnnotation annotation : this.editPart.getDiagramView().getEAnnotations()) {
 			toolSelectionTableViewer.add(annotation.getSource());
 			listPath.add(annotation.getSource());
 		}
-		
+
 	}
 
-		protected void addPredefiniedModelConfigurationFromPreferences(Combo combo) {
-	    	IConfigurationElement[] conf = Platform.getExtensionRegistry()
-					.getConfigurationElementsFor(
-							"org.eclipse.papyrus.dgts.preferences.DGTSPreferences");
-			String resource = null;
-			String ID = null;
-			for (IConfigurationElement c : conf) {
-				resource = c.getAttribute("resource");
-				ID = c.getAttribute("ID");
-				if (resource != null && ID != null) {
-					Bundle bundle = Platform
-							.getBundle(c.getContributor().getName());
-					URL url = bundle.getEntry(resource);
-					File file = null;
-					try {
-						file = new File(FileLocator.resolve(url).toURI());
-					} catch (URISyntaxException | IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					combo.add(file.getAbsolutePath());
-
+	protected void addPredefiniedModelConfigurationFromPreferences(Combo combo) {
+		IConfigurationElement[] conf = Platform.getExtensionRegistry().getConfigurationElementsFor("org.eclipse.papyrus.dgts.preferences.DGTSPreferences");
+		String resource = null;
+		String ID = null;
+		for(IConfigurationElement c : conf) {
+			resource = c.getAttribute("resource");
+			ID = c.getAttribute("ID");
+			if(resource != null && ID != null) {
+				Bundle bundle = Platform.getBundle(c.getContributor().getName());
+				URL url = bundle.getEntry(resource);
+				File file = null;
+				try {
+					file = new File(FileLocator.resolve(url).toURI());
+				} catch (URISyntaxException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
+				combo.add(file.getAbsolutePath());
+
 			}
-		
+		}
+
 	}
 
-		private static Group createGroup(Composite parent, String name) {
+	private static Group createGroup(Composite parent, String name) {
 
-	    	Group group = new Group(parent, SWT.NONE);
-	    	group.setText(name);
-	    	Color color = new Color(null, 255, 255, 255);
-	    	group.setBackground(color);
-	    	group.setBackground(color);
-	    	GridLayout layout = new GridLayout(1, false);
-	    	layout.marginHeight = 5;
-	    	layout.marginWidth = 5;
-	    	group.setLayout(layout);
-	    	GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-	    	group.setLayoutData(data);
-	    	return group;
-	        }
+		Group group = new Group(parent, SWT.NONE);
+		group.setText(name);
+		Color color = new Color(null, 255, 255, 255);
+		group.setBackground(color);
+		group.setBackground(color);
+		GridLayout layout = new GridLayout(1, false);
+		layout.marginHeight = 5;
+		layout.marginWidth = 5;
+		group.setLayout(layout);
+		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
+		group.setLayoutData(data);
+		return group;
+	}
 
 
-	public List<String> getAllModelConfigurationPath(){
-		return listPath ;
-		
+	public List<String> getAllModelConfigurationPath() {
+		return listPath;
+
 	}
 
 }
